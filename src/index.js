143f8d340e84967f7cbf29264fe2eca414abf85d
Agroecologia Cristobal Hernandez <octavoconjunto@gmail.com>
2025-07-14 13:31:13 -0400

    craecion y conexion de archivo index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'; // <--- AÑADE ESTA LÍNEA
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
